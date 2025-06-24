ce98e618104dab69d1c13c66ace48cd973316b9c
Faith Mulife <faithmulife093@gmail.com>
2025-06-24 16:50:42 +0200

    Delete arithmatic directory

--- arithmatic/xyArithmatic.php
+++
@@ -1,9 +0,0 @@
- <?php 
- $x=3;
- $y=6;
-
- echo  "the sum of x and y is: ". $x +$y;
- echo "the division of x and y is: ".$y/$x;
- echo "the multiplication of x and y is: ".$x*$y;
- echo "the difference of x and y is: ".$y-$x;
-?>
