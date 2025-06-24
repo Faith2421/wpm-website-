ef1f03272963fb6959790617c095c2e9ca73853f
Faith Mulife <faithmulife093@gmail.com>
2025-06-24 16:49:33 +0200

    Delete testPrep directory

--- testPrep/testPrepp.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$name=isset($_POST['name']) ? $_POST['name']: '';
-$Gender=isset($_POST['female']) ? $_POST['female'] :'';
-$Gender=isset($_POST['Male']) ? $_POST['Male'] :'';
-$Feeling=isset($_POST['happy'])? $_POST['happy'] :'';
-$Feeling= isset($_POST['sad'])? $_POST['sad']: ''; 
-$moodNumber=isset($_POST['moodNumber'])? $_POST['moodNumber']: '';
